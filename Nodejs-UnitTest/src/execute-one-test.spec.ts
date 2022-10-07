de902bc89be744aeed78fb7d149e51dd771ddfa1
Jonathan Alejandro <lordgelsin26@gmail.com>
2022-10-07 12:34:37 -0500

    saving commands jest

---
+++ Nodejs-UnitTest/src/execute-one-test.spec.ts
@@ -0,0 +1,16 @@
+describe('VirtualAccountTransaction', () => {
+
+  //to execute only this test use only
+  test.only('Balance is not sufficient', async () => {
+    
+  });
+
+  //these tests will be excluded
+  test('Balance is not sufficient', async () => {
+    
+  });
+
+  test('Balance is not sufficient', async () => {
+    
+  });
+})
---
+++ commands.jest.ts
@@ -0,0 +1,7 @@
+/* 
+  to execute one test 
+    npm test -g debit-reversal-virtual-account.controller.spec
+
+    npm test -- debit-reversal-virtual-account.controller.spec
+
+*/
